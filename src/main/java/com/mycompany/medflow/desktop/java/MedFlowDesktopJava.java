8cdae75d30ce4f27c194149d9eb1535f622a3983
Ivan Rodrigues <ivan2g.rodriigues@gmail.com>
2026-01-28 20:14:18 -0300

    Criando o projeto no Netbeans

---
+++ src/main/java/com/mycompany/medflow/desktop/java/MedFlowDesktopJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.medflow.desktop.java;
+
+/**
+ *
+ * @author ivan
+ */
+public class MedFlowDesktopJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
